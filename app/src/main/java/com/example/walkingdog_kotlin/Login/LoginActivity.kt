f17279b30ad36e294a953a7d9525eb9486c2a18b
Park Joonsung <jspark2201@naver.com>
2020-05-02 01:33:00 +0900

    create bottomNavigationBar

--- app/src/main/java/com/example/walkingdog_kotlin/Login/LoginActivity.kt
+++ app/src/main/java/com/example/walkingdog_kotlin/Login/LoginActivity.kt
@@ -1,13 +1,22 @@
 package com.example.walkingdog_kotlin.Login
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.walkingdog_kotlin.MainActivity
 import com.example.walkingdog_kotlin.R
+import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        kakao_login_btn.setOnClickListener {
+            var intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
     }
+
 }
--- app/src/main/java/com/example/walkingdog_kotlin/MainActivity.kt
+++ app/src/main/java/com/example/walkingdog_kotlin/MainActivity.kt
@@ -2,11 +2,49 @@ package com.example.walkingdog_kotlin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import android.view.View
+import android.widget.Toast
+import com.example.walkingdog_kotlin.Login.ProfileFragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), BottomNavigationView.OnNavigationItemSelectedListener {
+
+    override fun onNavigationItemSelected(p0: MenuItem): Boolean {
+        when(p0.itemId) {
+            R.id.action_home -> {
+                var feedFragment = FeedFragment()
+                supportFragmentManager.beginTransaction().replace(R.id.main_content_layout, feedFragment)
+                    .commit()
+            }
+            R.id.action_Walking -> {
+                var checkFragment = CheckFragment()
+                supportFragmentManager.beginTransaction().replace(R.id.main_content_layout, checkFragment)
+                    .commit()
+            }
+            R.id.action_challenge -> {
+                var challengeFragment = ChallengeFragment()
+                supportFragmentManager.beginTransaction().replace(R.id.main_content_layout, challengeFragment)
+                    .commit()
+            }
+            R.id.action_profile -> {
+                var profileFragment = ProfileFragment()
+                supportFragmentManager.beginTransaction().replace(R.id.main_content_layout, profileFragment)
+                    .commit()
+            }
+        }
+        return true
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val bottomNavigationView = findViewById<View>(R.id.bottom_navigation) as BottomNavigationView
+        bottomNavigationView.setOnNavigationItemSelectedListener(this)
+
+        //set default screen
+        bottom_navigation.selectedItemId = R.id.action_home
     }
 }

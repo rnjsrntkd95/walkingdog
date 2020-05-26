b2a276da9c2f8781f55147f90c4a58a3bfcb9ca1
MJ <minjeog0@gmail.com>
2020-05-26 22:52:28 +0900

    statics

--- app/src/main/java/com/example/walkingdog_kotlin/Statics.kt
+++ app/src/main/java/com/example/walkingdog_kotlin/Statics.kt
@@ -2,9 +2,13 @@ package com.example.walkingdog_kotlin
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.walkingdog_kotlin.Walking.StaticsItem
+import com.example.walkingdog_kotlin.Statics_RVAdapter
+import kotlinx.android.synthetic.main.activity_my_photo.*
+import kotlinx.android.synthetic.main.activity_statics.*
 import org.eazegraph.lib.charts.ValueLineChart
 import org.eazegraph.lib.models.ValueLinePoint
-
 import org.eazegraph.lib.models.ValueLineSeries
 
 
@@ -13,9 +17,24 @@ import org.eazegraph.lib.models.ValueLineSeries
 
 class Statics : AppCompatActivity() {
 
+    var history_list = arrayListOf<StaticsItem>(
+
+        StaticsItem("Today","26.4", "00","32","22" )
+        ,StaticsItem("Yesterday","34.7","00","41","10")
+        , StaticsItem("2 days ago","50.1","01","05","36")
+        ,StaticsItem("3 days ago","42.3","00","56","45")
+    )
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_statics)
+
+        val historyAdapter=Statics_RVAdapter(this, history_list)
+        history_recyclerview.adapter=historyAdapter
+
+        val lm = LinearLayoutManager(this)
+        history_recyclerview.layoutManager = lm
+        history_recyclerview.setHasFixedSize(true)
 
         val mCubicValueLineChart =
             findViewById(R.id.linechart) as ValueLineChart
---
+++ app/src/main/java/com/example/walkingdog_kotlin/Walking/StaticsItem.kt
@@ -0,0 +1,5 @@
+package com.example.walkingdog_kotlin.Walking
+
+class StaticsItem(val date:String, val cal:String, val hour:String, val minutes:String, val sec:String ) {
+
+}

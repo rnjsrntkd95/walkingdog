33a311788358f39871cba0b9734bd3dc4b0255d1
Park Joonsung <jspark2201@naver.com>
2020-06-06 20:54:32 +0900

    create walking list

---
+++ app/src/main/java/com/example/walkingdog_kotlin/Walking/WalkingList.kt
@@ -0,0 +1,13 @@
+package com.example.walkingdog_kotlin.Walking
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.walkingdog_kotlin.R
+
+class WalkingList : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_walking_list)
+    }
+}

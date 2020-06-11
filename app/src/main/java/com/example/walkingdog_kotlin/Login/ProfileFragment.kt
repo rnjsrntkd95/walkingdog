38b831a4ee58a00ac662c81748289d14e57c7c16
Park Joonsung <jspark2201@naver.com>
2020-06-11 18:52:30 +0900

    01

--- app/src/main/java/com/example/walkingdog_kotlin/Login/ProfileFragment.kt
+++ app/src/main/java/com/example/walkingdog_kotlin/Login/ProfileFragment.kt
@@ -34,7 +34,6 @@ class ProfileFragment : Fragment() {
     private val OPEN_GALLERY = 0
 
 
-
     private lateinit var firebaseAuth: FirebaseAuth
 
 
@@ -69,22 +68,19 @@ class ProfileFragment : Fragment() {
 
         super.onActivityCreated(savedInstanceState)
 
-
-        var sum_kcal = view?.findViewById<TextView>(R.id.profile_name)
-
-        statics.setOnClickListener {
+        walkingStaticLayout.setOnClickListener {
             val intent = Intent(context, Statics::class.java)
             startActivity(intent)
         }
 
-        myChallenge_layout.setOnClickListener {
+        myChallengeLayout.setOnClickListener {
             val pref = context?.getSharedPreferences("pref", Context.MODE_PRIVATE)
             val userToken = pref?.getString("userToken", "")
 
             super.onActivityCreated(savedInstanceState)
 
             walkingStaticLayout.setOnClickListener {
-                val intent =Intent(context,Statics::class.java)
+                val intent = Intent(context, Statics::class.java)
                 startActivity(intent)
             }
 
@@ -92,58 +88,59 @@ class ProfileFragment : Fragment() {
                 val pref = context?.getSharedPreferences("pref", Context.MODE_PRIVATE)
                 val userToken = pref?.getString("userToken", "5ebac6bd59e3d32080d6d941")
 
-                val challengeDetailRetrofit = ChallengeRetrofitCreators(context!!).ChallengeRetrofitCreator()
-                challengeDetailRetrofit.getMyChallenge(userToken!!).enqueue(object : Callback<myChallengeId> {
-                    override fun onFailure(call: Call<myChallengeId>, t: Throwable) {
-                        Log.d("DEBUG", " Challenge Retrofit failed!!")
-                        Log.d("DEBUG", t.message)
-                    }
-
-                    override fun onResponse(
-                        call: Call<myChallengeId>,
-                        response: Response<myChallengeId>
-                    ) {
-                        val challenge = response.body()?.myChallenge!![0].challengeId
-                        if (challenge == null) {
-                            (activity as MainActivity).bottom_navigation.selectedItemId =
-                                R.id.action_challenge
-                        } else {
-                            val intent = Intent(context, MyChallengeActivity::class.java)
-                            intent.putExtra("challengeId", challenge)
-                            startActivity(intent)
+                val challengeDetailRetrofit =
+                    ChallengeRetrofitCreators(context!!).ChallengeRetrofitCreator()
+                challengeDetailRetrofit.getMyChallenge(userToken!!)
+                    .enqueue(object : Callback<myChallengeId> {
+                        override fun onFailure(call: Call<myChallengeId>, t: Throwable) {
+                            Log.d("DEBUG", " Challenge Retrofit failed!!")
+                            Log.d("DEBUG", t.message)
                         }
-                    }
-                })
-        }
+
+                        override fun onResponse(
+                            call: Call<myChallengeId>,
+                            response: Response<myChallengeId>
+                        ) {
+                            val challenge = response.body()?.myChallenge!![0].challengeId
+                            if (challenge == null) {
+                                (activity as MainActivity).bottom_navigation.selectedItemId =
+                                    R.id.action_challenge
+                            } else {
+                                val intent = Intent(context, MyChallengeActivity::class.java)
+                                intent.putExtra("challengeId", challenge)
+                                startActivity(intent)
+                            }
+                        }
+                    })
+            }
 
 //            settings.setOnClickListener {
 //                val intent =Intent(context,Settings::class.java)
 //                startActivity(intent)
 //            }
-        myprofile_img_btn.setOnClickListener() {
+            myprofile_img_btn.setOnClickListener() {
 
-            loadImage()
+                loadImage()
+            }
+
+            logoutLayout.setOnClickListener {
+                val pref = context!!.getSharedPreferences("pref", Context.MODE_PRIVATE)
+                val edit = pref.edit()
+                edit.remove("userToken")
+                edit.apply()
+
+                val intent = Intent(context, LoginActivity::class.java)
+                startActivity(intent)
+                Toast.makeText(context, "로그아웃 되었습니다.", Toast.LENGTH_LONG).show()
+            }
+
+            add_pet_btn.setOnClickListener {
+                var intent = Intent(context, ProfileAddPetActivity::class.java)
+                startActivity(intent)
+            }
+
         }
-
-        logout_btn.setOnClickListener {
-            val pref = context!!.getSharedPreferences("pref", Context.MODE_PRIVATE)
-            val edit = pref.edit()
-            edit.remove("userToken")
-            edit.apply()
-
-            val intent = Intent(context, LoginActivity::class.java)
-            startActivity(intent)
-            Toast.makeText(context, "로그아웃 되었습니다.", Toast.LENGTH_LONG).show()
-        }
-
-        add_pet_btn.setOnClickListener {
-            var intent = Intent(context, ProfileAddPetActivity::class.java)
-            startActivity(intent)
-        }
-
-
     }
-
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
 
@@ -154,7 +151,10 @@ class ProfileFragment : Fragment() {
 
                 try {
                     val bitmap =
-                        MediaStore.Images.Media.getBitmap(context?.contentResolver, currentImageUrl)
+                        MediaStore.Images.Media.getBitmap(
+                            context?.contentResolver,
+                            currentImageUrl
+                        )
                     myprofile_img_btn.setImageBitmap(bitmap)
                 } catch (e: Exception) {
                     e.printStackTrace()
@@ -165,12 +165,11 @@ class ProfileFragment : Fragment() {
         }
     }
 
-    private fun loadImage() {
+    fun loadImage() {
         val intent = Intent()
         intent.type = "image/*"
         intent.action = Intent.ACTION_GET_CONTENT
         startActivityForResult(Intent.createChooser(intent, "Load Picture"), OPEN_GALLERY)
     }
+
 }
-
-
